6b35cd2a59fc736f0f888cca7aa8d04cc734f91f
FelixManFongCheung <manfongcheung74@gmail.com>
2021-05-22 14:44:15 +0800

    init

---
+++ ryan/src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import HeaderStyle from  './scss/Header.sass';
+
+function Header() {
+    <div className="header">
+        <h1 id="label">Ryan</h1>
+        <h1></h1>
+    </div>
+};
+
+export default Header;
